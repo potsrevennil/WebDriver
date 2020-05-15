bfb470fb44fed4f944a0d3d14b90401a54230024
Lim Thing-han <b02901144@gmail.com>
2020-05-15 16:52:55 +0800

    Refactor commands with mkRequest, sendRequest, doCommand. getCurrentUrl.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings, PartialTypeSignatures, FlexibleContexts #-}
-module Main where
 
 import Network.HTTP.Client
 import Control.Monad.Trans.State.Lazy
@@ -9,7 +8,7 @@ import Sessions
 main :: IO ()
 main = do 
     manager <- newManager defaultManagerSettings 
-    res <- evalStateT (getSessState (newSession >> navigateTo "http://google.com.tw" >> delSession))
+    res <- evalStateT (getSessState (newSession >> navigateTo "http://google.com" >> getCurrentUrl))
         (Session {sessHost = "127.0.0.1", sessPort = 4444, sessId = Nothing, sessManager = manager})
     print res
 
--- src/Commands.hs
+++ src/Commands.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE OverloadedStrings, PartialTypeSignatures, DeriveGeneric, FlexibleContexts, RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings, PartialTypeSignatures, DeriveGeneric, FlexibleContexts, NamedFieldPuns, RecordWildCards #-}
 module Commands (
     getSessionId,
     parseByteString,
     mkRequest,
+    sendRequest,
     newSession,
     delSession,
     navigateTo,
+    getCurrentUrl,
     getStatus
 )where
 
@@ -21,9 +23,11 @@ import Data.ByteString (ByteString, append)
 import Data.Typeable
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
+import Data.Maybe (fromMaybe)
 import Control.Monad.Base
+import Control.Exception (Exception)
 import Control.Exception.Lifted
-import Control.Monad.IO.Class
+
 import Control.Monad.Trans.State.Lazy
 import GHC.Generics
 import Capabilities
@@ -49,17 +53,18 @@ instance Exception BadJSON
 newtype BadJSON = BadJSON String
              deriving (Eq, Show, Typeable)
 
+parseAesonResult :: (MonadBase IO m, FromJSON a) => Data.Aeson.Types.Result a -> m a
+parseAesonResult (Success val) = return val
+parseAesonResult (Error err) = throwIO $ BadJSON err
+
 parseByteString :: (MonadBase IO m, FromJSON a) => LB.ByteString -> m a
 parseByteString s = case AP.parse json s of
-                Done _ res -> 
-                    case fromJSON res of
-                        Success val -> return val
-                        Error err -> throwIO $ BadJSON err
+                Done _ res -> parseAesonResult (fromJSON res)
                 Fail _ _ err   -> throwIO $ BadJSON err
 
 
-mkRequest :: Method -> ByteString -> RequestBody -> Request
-mkRequest meth path args = defaultRequest {
+mkRequest :: Method -> ByteString -> LB.ByteString -> SessState Request
+mkRequest meth path args = return defaultRequest {
         host = "127.0.0.1",
         port = 4444,
         secure = False,
@@ -72,43 +77,74 @@ mkRequest meth path args = defaultRequest {
                         ],    
         path = "/wd/hub/" `append` path,
         method = meth,
-        requestBody = args
+        requestBody = RequestBodyLBS args
     }
+
+sendRequest :: Request -> SessState (Response LB.ByteString)
+sendRequest req = do
+    s@Session {..} <- SessState get
+    liftBase (httpLbs req sessManager)
+    
+
+data HTTPStatusUnknown = HTTPStatusUnknown Int ByteString deriving (Eq, Show, Typeable)
+instance Exception HTTPStatusUnknown
+
+updateSessionId :: ResponseMes -> SessState ()
+updateSessionId ResponseMes {..} = do
+    s@Session {..} <- SessState get
+    case (sessId , sessionId) of
+        (_, Nothing) -> return ()
+        (_, Just i) -> SessState (put (s {sessId = (Just . encodeUtf8 . sessionIdText) i}))
+
+parseResBodyStatus :: (Exception e) => ResponseMes -> SessState (Maybe e)
+parseResBodyStatus ResponseMes {status = 0} = return Nothing
+parseResBodyStatus _ = return (Just (error "Error"))
+
+parseResponse :: (FromJSON a, Exception e) => Response LB.ByteString -> SessState (Either e a)
+parseResponse res 
+    | status == 200 = 
+        if LB.null body
+            then Right <$> (parseAesonResult . fromJSON $ Null)
+            else do
+                resMsg@ResponseMes { value } <- parseByteString body
+                parseResBodyStatus resMsg >>= maybe
+                    (updateSessionId resMsg >> Right <$> parseAesonResult (fromJSON value))
+                    (return . Left)
+    | otherwise =  return . Left . error $ "Error"
+        where 
+            status = statusCode (responseStatus res) 
+            body = responseBody res
+
+doCommand :: Method -> ByteString -> LB.ByteString -> SessState (Response LB.ByteString)
+doCommand meth path args = mkRequest meth path args >>= sendRequest
 
 newSession :: SessState (Response LB.ByteString)
 newSession = do
+    res <- doCommand methodPost "session" $ (encode . toJSON . object) ["desiredCapabilities" .= defCapabilities]
     s@Session {..} <- SessState get
-    res <- liftBase (httpLbs req sessManager)
     si <- getSessionId res
     SessState (put (s {sessId = si}))
     return res
-    where req = (mkRequest methodPost "session" . RequestBodyLBS . encode . toJSON . object) 
-                    ["desiredCapabilities" .= defCapabilities]
-
 
 delSession :: SessState (Response LB.ByteString)
 delSession = do
-    s@Session { .. } <- SessState get
-    SessState (put (s {sessId = Nothing}))
-    liftBase (httpLbs (req sessId) sessManager)
-        where 
-            req sid = (mkRequest methodDelete (path sid) . RequestBodyLBS) ""
-                where
-                    path :: Maybe ByteString -> ByteString
-                    path (Just i) = "session/" `append` i
-                    path Nothing = "session/"
+    Session { .. } <- SessState get
+    doCommand methodDelete ("session/" `append` fromMaybe "" sessId) ""
 
 navigateTo :: Text -> SessState (Response LB.ByteString)
 navigateTo url = do
-    s@Session { .. } <- SessState get
-    liftBase (httpLbs (req sessId) sessManager)
-    where 
-        req sid = (mkRequest methodPost (path sid) . RequestBodyLBS . encode .toJSON .object) ["url" .= url]
-            where
-                path :: Maybe ByteString -> ByteString
-                path (Just i) = "session/" `append` i `append` "/url"
-                path Nothing = "session/url"
-        
-getStatus :: IO (Response LB.ByteString)
-getStatus = newManager defaultManagerSettings >>= httpLbs req
-    where req = (mkRequest methodGet "status" . RequestBodyLBS) ""
+    Session { .. } <- SessState get
+    doCommand methodPost ("session/" `append` fromMaybe "" sessId `append` "/url") $ (encode .toJSON .object) ["url" .= url]
+
+getCurrentUrl :: SessState String
+getCurrentUrl = do
+    Session { .. } <- SessState get
+    res <- doCommand methodGet ("session/" `append` fromMaybe "" sessId `append` "/url") ""
+    ResponseMes { .. } <- parseByteString (responseBody res)
+    case fromJSON value of
+        Success val -> return val
+        Error err -> throwIO $ BadJSON err
+
+getStatus :: SessState (Response LB.ByteString)
+getStatus = doCommand methodGet "status" ""
+
